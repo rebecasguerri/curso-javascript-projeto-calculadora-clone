04e6042fd6b1fc259945e235da3fd8395efbdb18
rebecasguerri <rebecasguerri@gmail.com>
2022-07-13 17:00:12 -0300

    12

---
+++ controler/calcontroller.js
@@ -0,0 +1,70 @@
+ class CalController{
+    
+    constructor(){
+        this._locale = 'pt-BR'
+        this._displayCalcEl = document.querySelector("#display");
+        this._dateEl = document.querySelector("#data");
+        this._timeEl = document.querySelector("#hora");
+        this.displayCalc = "0";
+        this._currentDate;
+        this.initialize();
+        this.initButtonsEvents();
+    }
+    initialize(){
+       this.setDisplayDateTime();
+
+        setInterval(() =>{
+           this.setDisplayDateTime();
+        }, 1000);
+    }
+
+    initButtonsEvents(){
+       let buttons = document.querySelectorAll("#buttons >g, #parts > g");
+
+       buttons.forEach((btn, index) =>{ 
+
+            btn.addEventListener('click', e=>{
+             console.log(btn.className.baseVal.replace("btn-", ""));
+        });
+    })
+       
+          
+
+       
+    }
+
+
+    setDisplayDateTime(){
+        this.displayDate =this.currentDate.toLocaleDateString(this._locale)
+        this.displayTime =this.currentDate.toLocaleTimeString(this._locale)
+    }
+
+    get displayTime(){
+        return this._timeEl.innerHTML;
+    }
+    set displayTime(value){
+        return this._timeEl.innerHTML= value;
+    }
+
+    get displayDate(){
+        return this._dateEl.innerHTML;
+    }
+    set displayDate(value){
+        return this._dateEl.innerHTML = value;
+    }
+
+    get displayCalc(){
+        return this._displayCalcEl.innerHTML;
+    }
+
+    set displayCalc(valor){
+        this._displayCalcEl.innerHTML = valor;
+    }
+
+    get currentDate(){
+        return new Date();
+    }
+    set currentDate(valor){
+        this.currentDate = valor;
+    }
+}
